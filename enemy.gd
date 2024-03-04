f8ea5699eb1835fabe3f6ab0aefe9876cdb3c92d
Jesse Rhodes <l337rhodes@gmail.com>
2024-03-03 22:58:03 -0500

    initial enemy wip

---
+++ enemy.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+signal player_hit
+
+var playerNode
+var speed = 60
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	playerNode = get_node("/root/main/player")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	velocity = (playerNode.position - position).normalized() * delta * speed
+	var collided = move_and_collide(velocity)
+	if collided != null:
+		if collided.get_collider().name == "player":
+			#emit_signal(player_hit)
+			print("player hit!")
+			# start invulnerability timer
